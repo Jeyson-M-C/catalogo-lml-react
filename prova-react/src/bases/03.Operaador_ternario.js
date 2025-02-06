812046cf19ab657526978e8afc54e1f0be2d3633
Jeyson-M-C <jeyson.moran@outlook.com>
2025-02-05 18:21:19 -0600

    catalogo-lml-react-01

--- prova-react/src/bases/03.Operaador_ternario.js
+++
@@ -1,9 +0,0 @@
-const activo = true;
-const v = true;
-
-//const mensaje = ( activo ) ? 'Activo' : 'Inactivo';
-const mensaje = !activo && 'Activo'; 
-const msj = ( v === true) && 'Verdadero'
-
-console.log(mensaje)
-console.log(msj)
